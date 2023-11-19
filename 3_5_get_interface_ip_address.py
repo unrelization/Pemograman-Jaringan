d2eda020dd27a62716198d34947f98e9f84388c5
Crystal Oscillator <78525180+unreliablecode@users.noreply.github.com>
2023-11-19 22:26:29 +0700

    Create 3_5_get_interface_ip_address.py

---
+++ 3_5_get_interface_ip_address.py
@@ -0,0 +1,16 @@
+import argparse
+import sys
+import socket
+import fcntl
+import struct
+import array
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(s.fileno(),0x8915,struct.pack(b'256s', bytes(ifname[:15], 'utf-8')))[20:24])
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Python networking utils')
+    parser.add_argument('--ifname', action="store",
+    dest="ifname", required=True)
+    given_args = parser.parse_args()
+    ifname = given_args.ifname
+    print ("Interface [%s] --> IP: %s" %(ifname, get_ip_address(ifname)))
